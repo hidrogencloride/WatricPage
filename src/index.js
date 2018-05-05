eb6bc5fae2f9dfe792523c98ed7a1b5a567e98d9
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-05-05 01:03:19 -0400

    all setup finished

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {hashHistory , Router, Route} from 'react-router'
+
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /bootstrap\.native/,
+          use: {
+            loader: 'bootstrap.native-loader'
+          }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
