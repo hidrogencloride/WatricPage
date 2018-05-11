594673988be28f41ff65ded53dd906c308a1cd0b
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-05-10 23:13:03 -0400

    Fixed config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path =  require("path");
+var path = require('path');
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
 
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
+                use: {
+                    loader: 'url-loader?limit=30000&name=images/[name].[ext]'
+                }
+            },
+            {
+                test: /bootstrap\.native/,
+                use: {
+                    loader: 'bootstrap.native-loader'
+                }
+            }
         ]
-      },
-      {
-        test: /bootstrap\.native/,
-          use: {
-            loader: 'bootstrap.native-loader'
-          }
-      },
-        {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            include: "./src/static/images",
-            loader  : 'url-loader?limit=30000&name=images/[name].[ext]'
-        },
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
