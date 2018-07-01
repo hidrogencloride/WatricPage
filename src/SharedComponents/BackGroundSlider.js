1ee12bf109891aa52c0d3770ecc5151a9d92a885
Irixa Vales <irixa.vales@upr.edu>
2018-07-01 00:26:20 -0400

    Added moving slogan and page title

--- src/SharedComponents/BackGroundSlider.js
+++ src/SharedComponents/BackGroundSlider.js
@@ -10,22 +10,55 @@ import Test from '../static/images/godafoss-1840758.jpg'
 
 export default class BackGroundSlider extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            current_word: "1"
+        }
+    }
+
+    changeState(word){
+        this.setState({
+            current_word: word
+        })
+    }
 
     render(){
         return(
-                <div id = "BackgroundSlider" style={{height:507}}>
-                    <Carousel controls={false} className={"back-slider"}>
-                        <Carousel.Item>
-                            <img className={"img-responsive"}  alt="900x500" src={test} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className={"img-responsive"} src={cliff} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className={"img-responsive"}  alt="900x500" src={Test} />
-                        </Carousel.Item>
-                    </Carousel>
+            <div class="row">
+                <div class="col-md-12">
+                    <div id = "BackgroundSlider" style={{height:507}}>
+                        <Carousel controls={false} className={"back-slider"}>
+                            <Carousel.Item>
+                                <img className={"img-responsive"}  alt="900x500" src={test} />
+                                    <Carousel.Caption>
+                                        <h3>
+                                            energy
+                                        </h3>
+                                    </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className={"img-responsive"} src={cliff} />
+                                <Carousel.Caption>
+                                    <h3>
+                                        water
+                                    </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </div>
+
+                    <div id={"slogan"}>
+                        <div class="col-md-12 text-center">
+                            <h3>
+                                <p>Creating {status.current_word}
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                out of thin air</p>
+                            </h3>
+                        </div>
+                    </div>
                 </div>
+            </div>
 
         );
     }
